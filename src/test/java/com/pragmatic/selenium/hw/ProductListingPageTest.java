bcf23049edef00a1f576da60a544353dec43d6a1
Sanuri Perera <sanuriperera98@gmail.com>
2025-01-22 15:14:56 +0530

    Update Product list page

--- src/test/java/com/pragmatic/selenium/hw/ProductListingPageTest.java
+++ src/test/java/com/pragmatic/selenium/hw/ProductListingPageTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -24,8 +25,40 @@ public class ProductListingPageTest {
 //    }
 
     @Test
-    public void testVerifyAllProductDisplay(){
-        // Get all product elements 
-        List<WebElement> productContainers = webDriver.findElements(By.className("product-container"));
+    public void testVerifyProductDetails(){
+        //Test Case 2.1: Verify if all products are displayed with the correct name, price, and image.
+        webDriver.findElement(By.id("user-name")).sendKeys("standard_user");
+        webDriver.findElement(By.id("password")).sendKeys("secret_sauce");
+        webDriver.findElement(By.id("login-button")).click();
+        Assert.assertEquals(webDriver.findElement(By.cssSelector("[data-test ='title']")).getText(),"Products");
+        // Expected product details (using a 2D array)
+        Object[][] expectedProducts = {
+                {"Sauce Labs Backpack", "$29.99", "sauce-backpack-1200x1500.0a0b85a3.jpg"},
+                {"Sauce Labs Bike Light", "$9.99", "bike-light-1200x1500.37c843b0.jpg"},
+                {"Sauce Labs Bolt T-Shirt", "$15.99", "bolt-shirt-1200x1500.c2599ac5.jpg"},
+                {"Sauce Labs Fleece Jacket", "$49.99", "sauce-pullover-1200x1500.51d7ffaf.jpg"},
+                {"Sauce Labs Onesie", "$7.99", "red-onesie-1200x1500.2ec615b2.jpg"},
+                {"Test.allTheThings() T-Shirt (Red)", "$15.99", "red-tatt-1200x1500.30dadef4.jpg"}
+        };
+        // Get all product elements
+        List<WebElement> productElements = webDriver.findElements(By.cssSelector("[data-test='inventory-item']"));
+        for (WebElement product : productElements) {
+            String productName = product.findElement(By.className("inventory_item_name")).getText();
+            String productPrice = product.findElement(By.className("inventory_item_price")).getText();
+            String productImage = product.findElement(By.xpath("//div[@class= 'inventory_item_img']/a/img")).getDomProperty("src");
+            System.out.println("Product Name: " + productName);
+            System.out.println("Product Price: " + productPrice);
+            System.out.println("product image:" + productImage);
+            System.out.println("---------------------");
+            // Find the expected details for the current product
+            boolean found = false;
+            for (Object[] expectedDetails : expectedProducts) {
+                Assert.assertEquals(productName,expectedDetails[0],"Product name mismatch for " + productName);
+//               // Assert.assertEquals(productPrice, expectedDetails[1], "Product price mismatch for " + productName);
+////                    Assert.assertEquals(productImage, expectedDetails[2], "Product image mismatch for " + productName);
+            }
+        }
+
+
     }
 }
